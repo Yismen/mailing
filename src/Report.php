12e795428009a5feae8aaf41d770e41c9631e22a
Yismen Jorge <yismen.jorge@gmail.com>
2022-12-19 15:35:24 -0400

    refactor: :recycle: Throw specific exceptions

--- src/Report.php
+++ src/Report.php
@@ -5,6 +5,9 @@ namespace Dainsys\Report;
 use Illuminate\Mail\Mailable;
 use Dainsys\Report\Contracts\ReportContract;
 use Dainsys\Report\Models\Mailable as ModelsMailable;
+use Dainsys\Report\Exceptions\MailableNotFoundException;
+use Dainsys\Report\Exceptions\MailableNotActiveException;
+use Dainsys\Report\Exceptions\MailableWithoutRecipientsException;
 
 class Report implements ReportContract
 {
@@ -26,8 +29,18 @@ class Report implements ReportContract
             ->with(['recipients'])
             ->first();
 
-        return $report
-            ? $report->recipients->pluck('email', 'name')->all()
-            : [];
+        if (!$report) {
+            throw new MailableNotFoundException("Unable to find a record for report {$class_name}. Did you added it to the mailables table in route /report/admin/mailables?", 404);
+        }
+
+        if (!$report->active) {
+            throw new MailableNotActiveException("Mailable {$class_name} is inactive, therefore should not be receiving emails.", 419);
+        }
+
+        if ($report->recipients->count() === 0) {
+            throw new MailableWithoutRecipientsException("There is no recipients assigned to report {$class_name} so no one to send email to. Please assign some in route /report/admin/mailables!", 405);
+        }
+
+        return  $report->recipients->pluck('email', 'name')->all();
     }
 }
